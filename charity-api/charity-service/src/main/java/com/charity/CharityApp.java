36538edc77f51763632e8021d8989db08af3d1aa
Filip7243 <filip7243@gmail.com>
2025-05-11 13:43:14 +0200

    h2 settings done

---
+++ charity-api/charity-service/src/main/java/com/charity/CharityApp.java
@@ -0,0 +1,11 @@
+package com.charity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CharityApp.class, args);
+    }
+}
